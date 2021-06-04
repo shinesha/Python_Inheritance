ec5b67749ae0bc770286d41ce24a2687f7191d4c
shinesha <jodrew_7@hotmail.com>
2021-06-04 14:08:06 +0100

    hackerank find a string problem

--- venv/Hacker_kattas.py
+++ venv/Hacker_kattas.py
@@ -4,3 +4,11 @@ def mutate_string(string, position, character):
     l[position] = character
     string = ''.join(l)
     return string
+
+def count_substring(string, sub_string):
+    count=0
+    #for j in range (0, len(sub_string)):
+    for i in range (len(string)):
+        if string[i:i+(len(sub_string))]==sub_string:
+            count +=1
+    return count
